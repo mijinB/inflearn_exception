6a00e8be447fb91d1303e55f65aa6b0bdb9fe085
mijinB <bmj13465@gmail.com>
2025-05-15 13:59:09 +0900

    서블릿 예외 처리 - 인터셉터

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -1,17 +1,28 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");   // 어떤 DispatcherTypes 인 경우에 적용할 지 선택하는 기능이 없다. 대신 excludePathPatterns 을 사용하여 요청 경로에 따라서 추가하거나 제외하기 쉽게 되어 있다.
+                                                                                        // excludePathPatterns 에서 "/error-page/**" 를 제거하면 error-page/500 같은 내부 호출의 경우에도 인터셉터가 호출된다.
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
